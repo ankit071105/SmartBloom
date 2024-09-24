07788bca5a4ccfd2816a8895514aeceab8e7922e
Ankit Kumar <139707943+ankit071105@users.noreply.github.com>
2024-09-24 22:55:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Toggle Nav Menu
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+
+// Scroll to Top Button
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+});
